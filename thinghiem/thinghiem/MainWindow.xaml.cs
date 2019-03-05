05d58df7ef19cde81354f864d1c7d132eee8d8ac
tuan le <lehoangtuan1381997a@gmail.com>
2019-03-05 10:46:44 +0700

    cap nhat

--- thinghiem/thinghiem/MainWindow.xaml.cs
+++ thinghiem/thinghiem/MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace thinghiem
         {
             MessageBox.Show(
                "thông báo",
-               "chào các bạn 2"
+               "chào các bạn 2 mình là người mới "
                );
         }
     }
